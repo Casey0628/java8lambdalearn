c238f73e29eb1e61557148f57b3bac7be77c5714
C5310928 <kaixi.niu@sap.com>
2022-05-16 14:17:52 +0800

    update method param

--- src/test/java/com/nkx/lambda/TestLambda2.java
+++ src/test/java/com/nkx/lambda/TestLambda2.java
@@ -70,7 +70,7 @@ public class TestLambda2 {
 	@Test
 	public void test2(){
 		Consumer<String> con = x -> System.out.println(x);
-		con.accept("我大尚硅谷威武！");
+		con.accept("我爱学java!");
 	}
 	
 	@Test
--- src/test/java/com/nkx/lambda/TestLambda3.java
+++ src/test/java/com/nkx/lambda/TestLambda3.java
@@ -55,10 +55,10 @@ public class TestLambda3 {
 	//Function<T, R> 函数型接口：
 	@Test
 	public void test3(){
-		String newStr = strHandler("\t\t\t 我大尚硅谷威武   ", (str) -> str.trim());
+		String newStr = strHandler("\t\t\t 中国共产党万岁   ", (str) -> str.trim());
 		System.out.println(newStr);
 		
-		String subStr = strHandler("我大尚硅谷威武", (str) -> str.substring(2, 5));
+		String subStr = strHandler("中国共产党万岁", (str) -> str.substring(2, 5));
 		System.out.println(subStr);
 	}
 	
